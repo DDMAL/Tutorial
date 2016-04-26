1d3320afc53223b3fa7a608a8e836c5b2f86ac31
Andrew Hankinson <andrew.hankinson@gmail.com>
2016-04-26 17:00:04 -0400

    Fixed: Compositions have titles, not names

--- catalogue/models/composition.py
+++ catalogue/models/composition.py
@@ -5,6 +5,6 @@ class Composition(models.Model):
     class Meta:
         app_label = "catalogue"
 
-    name = models.CharField(max_length=64)
+    title = models.CharField(max_length=64)
     composer = models.ForeignKey("catalogue.Composer", blank=True, null=True)
     anonymous = models.BooleanField(default=False)
