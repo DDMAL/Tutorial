ce358fb1bb23c4e3c57be501e46aeeba2e56ac1f
Andrew Hankinson <andrew.hankinson@gmail.com>
2016-04-27 20:56:07 -0400

    Fixed: Made fields on source nullable
    This was done to make the importing process easier.

--- catalogue/models/source.py
+++ catalogue/models/source.py
@@ -8,11 +8,11 @@ class Source(models.Model):
     shelfmark = models.CharField(max_length=255)
     name = models.CharField(max_length=255, blank=True, null=True)
     surface = models.CharField(max_length=128, blank=True, null=True)
-    start_date = models.IntegerField()
-    end_date = models.IntegerField()
+    start_date = models.IntegerField(blank=True, null=True)
+    end_date = models.IntegerField(blank=True, null=True)
     date_comments = models.TextField(blank=True, null=True)
     type = models.CharField(max_length=128, blank=True, null=True)
-    archive = models.ForeignKey("catalogue.Archive")
+    archive = models.ForeignKey("catalogue.Archive", blank=True, null=True)
 
     @property
     def display_name(self):
