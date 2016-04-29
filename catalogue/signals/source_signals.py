dbb3913fb520644617bd904d1ff95e207805fa1f
Andrew Hankinson <andrew.hankinson@gmail.com>
2016-04-29 16:42:57 -0400

    New: Indexing Source records on save/delete

--- catalogue/signals/source_signals.py
+++ catalogue/signals/source_signals.py
@@ -1,16 +1,17 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
 from catalogue.models.source import Source
+from catalogue.serializers.search.source import SourceSearchSerializer
+from catalogue.helpers.solr_helpers import solr_index, solr_delete
 
 
 @receiver(post_save, sender=Source)
 def index_source(sender, instance, created, **kwargs):
-    print('index source')
-    pass
+    solr_index(SourceSearchSerializer, [instance])
 
 
 @receiver(post_delete, sender=Source)
 def delete_source(sender, instance, **kwargs):
-    print('delete source')
-    pass
+    solr_delete([instance])
+
 
